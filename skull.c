a7cc0a084dc08273cb81f79f5f0eceb37949bb97
Singgih Febriana <64336941+singgihfebriana@users.noreply.github.com>
2020-09-22 09:43:21 +0700

    Add files via upload

---
+++ skull.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+
+main(){
+	int a,b,c,d,e,f,g,h,i;
+for (a=0;a<=2;a++){
+	printf(" ");
+}
+for (b=0;b<=2;b++){
+	printf("*");
+}
+printf("\n");
+printf(" ");
+printf("*");
+for(c=0;c<=4;c++){
+	printf(" ");
+}
+printf("*");
+printf("\n");
+printf(" ");
+printf("*");
+printf(" ");
+printf("8");
+printf(" ");
+printf("8");
+printf(" ");
+printf("*\n");
+for (d=0;d<=1;d++){
+	printf(" ");
+}
+printf("*");
+printf(" ");
+printf("0");
+printf(" ");
+printf("*");
+printf("\n");
+for (e=0;e<=2;e++){
+	printf(" ");
+}
+for (f=0;f<=2;f++){
+	printf("m");
+}
+printf("\n");
+for (g=0;g<=2;g++){
+	printf(" ");
+}
+for (h=0;h<=2;h++){
+	printf("w");
+}
+printf("\n");
+for(i=0;i<=3;i++){
+	printf(" ");
+}
+printf("*");
+}
+
+
